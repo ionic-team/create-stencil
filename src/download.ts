a41214747989548fda46d3935ef775a62f9b95ab
sautte_d <david.sautter@rohde-schwarz.com>
2019-05-14 12:25:20 +0200

    fix: support https_proxy variable

--- src/download.ts
+++ src/download.ts
@@ -1,26 +1,35 @@
-
 import { get } from 'https';
+import * as Url from 'url';
 import { Starter } from './starters';
 
+// tslint:disable-next-line:no-var-requires
+const HttpsProxyAgent = require('https-proxy-agent');
 
 export function downloadStarter(starter: Starter) {
-  return downloadFromURL(`https://github.com/${starter.repo}/archive/master.zip`);
+    return downloadFromURL(`https://github.com/${starter.repo}/archive/master.zip`);
 }
 
 function downloadFromURL(url: string): Promise<Buffer> {
-  return new Promise((resolve, reject) => {
-    get(url, (res) => {
-      if (res.statusCode === 302) {
-        downloadFromURL(res.headers.location!).then(resolve, reject);
-      } else {
-        const data: any[] = [];
 
-        res.on('data', chunk => data.push(chunk));
-        res.on('end', () => {
-          resolve(Buffer.concat(data));
+    const options = Url.parse(url);
+    const agent = new HttpsProxyAgent(process.env.https_proxy);
+
+    // @ts-ignore
+    options.agent = agent;
+
+    return new Promise((resolve, reject) => {
+        get(options, (res) => {
+            if (res.statusCode === 302) {
+                downloadFromURL(res.headers.location!).then(resolve, reject);
+            } else {
+                const data: any[] = [];
+
+                res.on('data', chunk => data.push(chunk));
+                res.on('end', () => {
+                    resolve(Buffer.concat(data));
+                });
+                res.on('error', reject);
+            }
         });
-        res.on('error', reject);
-      }
     });
-  });
 }
