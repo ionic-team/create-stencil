021f684dc3445d94f826be4dd5b7a69bf2bf3a8a
pwespi <pwespi@pm.me>
2019-11-22 04:18:26 +0100

    chore(): update PWA toolkit docs url (#23)

--- src/starters.ts
+++ src/starters.ts
@@ -12,7 +12,7 @@ export const STARTERS: Starter[] = [
     name: 'ionic-pwa',
     repo: 'ionic-team/ionic-pwa-toolkit',
     description: 'Everything you need to build fast, production ready PWAs',
-    docs: 'https://beta.ionicframework.com'
+    docs: 'https://ionicframework.com/enterprise/solutions/pwa'
   },
   {
     name: 'app',
