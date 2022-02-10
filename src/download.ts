67310477ddc633310d45689157e0069e5bfe0231
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-02-10 11:58:58 -0500

    chore(downloads): update path to starters
    update the path to the starters that we currently support. this is being
    done after moving each of their primary branches from 'master' to
    'main'. this change doesn't need to be rushed out into production, as
    redirects on github's side should suffice for some time

--- src/download.ts
+++ src/download.ts
@@ -4,7 +4,7 @@ import { Starter } from './starters';
 import * as HttpsProxyAgentModule from 'https-proxy-agent';
 
 export function downloadStarter(starter: Starter) {
-  return downloadFromURL(`https://github.com/${starter.repo}/archive/master.zip`);
+  return downloadFromURL(`https://github.com/${starter.repo}/archive/main.zip`);
 }
 
 function downloadFromURL(url: string): Promise<Buffer> {
