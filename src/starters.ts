fe567eecacef6dd20511745173db31eb12711783
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-13 16:52:20 -0400

    refector(jsdoc): document starters.ts

--- src/starters.ts
+++ src/starters.ts
@@ -1,12 +1,38 @@
+/**
+ * Metadata for a starter project that the CLI will use to bootstrap a user's project.
+ */
 export interface Starter {
+  /**
+   * The name of the starter.
+   */
   name: string;
+  /**
+   * The GitHub repository the starter can be found in. The base URL is assumed to exist and does not need to be
+   * provided.
+   */
   repo: string;
+  /**
+   * A brief description of the starter project.
+   */
   description?: string;
+  /**
+   * A link to the starter's documentation.
+   */
   docs?: string;
+  /**
+   * When true, the starter should be hidden from the list of possible choices. This allows the `name` to be used with
+   * the `npm init stencil component <NAME>` command, without cluttering the options list.
+   */
   hidden?: boolean;
+  /**
+   * When true, the starter is a community-driven project, rather than one owned by Ionic
+   */
   isCommunity?: boolean;
 }
 
+/**
+ * Existing Stencil project starters available for CLI users to select from
+ */
 export const STARTERS: Starter[] = [
   {
     name: 'component',
@@ -30,6 +56,14 @@ export const STARTERS: Starter[] = [
   },
 ];
 
+/**
+ * Retrieve a starter project's metadata based on a CLI user's input.
+ *
+ * @param starterName the name of the starter project to retrieve. Starter names that include a forward slash ('/') are
+ * assumed to be custom starter templates. Such templates are assumed to be the name of the repository that this CLI
+ * can retrieve the starter template from.
+ * @returns the starter project metadata
+ */
 export function getStarterRepo(starterName: string): Starter {
   if (starterName.includes('/')) {
     return {
