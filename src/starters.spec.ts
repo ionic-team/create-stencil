f073e86b660622063480ba43e6a2c8d1c45b6856
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-26 14:09:58 -0400

    chore(test): add testing to project (#75)
    add testing infrastructure to the project. ts-jest was picked due to its
    dominance in the ts ecosystem to transpile typescript to javascript
    (over, say babel). this decision was slightly arbitrary - no design doc
    or case study was performed in the making of this decision.
    
    we start with ts-jest's default configuration options, which we can tune
    as we go.
    
    this commit exercises the new infrastructure by getting `starters.ts`
    under test

---
+++ src/starters.spec.ts
@@ -0,0 +1,31 @@
+import { getStarterRepo, Starter } from './starters';
+
+describe('starters', () => {
+  describe('getStarterRepo', () => {
+    it('returns the correct starter info for a custom repo', () => {
+      const starterRepo = 'github.com/custom-stencil-starter';
+
+      const starterInfo = getStarterRepo(starterRepo);
+
+      expect(starterInfo).toEqual<Starter>({
+        name: starterRepo,
+        repo: starterRepo,
+      });
+    });
+
+    it('returns a known starter project when specified by name', () => {
+      const starterInfo = getStarterRepo('component');
+
+      expect(starterInfo).toEqual<Starter>({
+        description: 'Collection of web components that can be used anywhere',
+        docs: 'https://github.com/ionic-team/stencil-component-starter',
+        name: 'component',
+        repo: 'ionic-team/stencil-component-starter',
+      });
+    });
+
+    it('throws an error for an unknown starter project', () => {
+      expect(() => getStarterRepo('unknown')).toThrow('Starter "unknown" does not exist.');
+    });
+  });
+});
