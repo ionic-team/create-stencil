f4c2fc3e3b0f3c4b4b538c4ae595cf15a3115c38
Christian Bromann <git@bromann.dev>
2023-09-29 13:04:09 -0700

    fix(init): verify starter repo exists to avoid silent interruption (#361)
    * fix(init): verify starter repo exists to avoid silent interruption
    
    * pr feedback

---
+++ src/download.spec.ts
@@ -0,0 +1,23 @@
+import { verifyStarterExists } from './download';
+
+describe('download', () => {
+  describe('verifyStarterExists', () => {
+    it('returns false if starter does not exist', async () => {
+      expect(
+        await verifyStarterExists({
+          repo: 'foo/bar',
+          name: 'foo-bar-starter',
+        }),
+      ).toBe(false);
+    });
+
+    it('returns true if starter does exist', async () => {
+      expect(
+        await verifyStarterExists({
+          repo: 'ionic-team/stencil',
+          name: 'stencil',
+        }),
+      ).toBe(true);
+    });
+  });
+});
